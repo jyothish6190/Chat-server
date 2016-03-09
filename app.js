ccaf26e7c37be4c06f4b1ce4156130ff69b70eae
Jyothish <jyothish6190@gmail.com>
2016-03-09 19:28:00 +0530

    initial set up

---
+++ app.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Jyothish on 09-03-2016.
+ */
+
+var express  = require('express');
+var connect = require('connect');
+var app      = express();
+var port     = process.env.PORT || 8080;
+
+// Configuration
+app.use(express.static(__dirname + '/public'));
+app.use(connect.logger('dev'));
+app.use(connect.json());
+app.use(connect.urlencoded());
+
+// Routes
+
+require('./routes/routes.js')(app);
+
+app.listen(port);
+
+console.log('The App runs on port ' + port);
